c539145259b6e94559321e3d16e60a4b9f93fb84
YB School <youngbosohn@gmail.com>
2016-05-19 10:09:34 +0800

    Undo

--- Characters.js
+++ Characters.js
@@ -1,11 +1,3 @@
 $( document ).ready(function() {
-$("#C1").hide();
-$("#exploreBody").hide();
-$("#Start").click(function(){
-$("#C1").show();
-$("#Start").hide();
-$("#exploreBody").show();
-});
-
 
 });
