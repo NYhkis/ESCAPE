10a563f4ca4ca1036048d068d3b8b19dc4db389f
YB School <youngbosohn@gmail.com>
2016-05-23 10:26:00 +0800

    Undo

--- Characters.js
+++ Characters.js
@@ -169,8 +169,12 @@ function game() {
         ctx.shadowOffsetY = 0;
         ctx.shadowColor   = "#999";
 
-        ctx.rect(
-
+        ctx.arc(
+            (cW/2),
+            (cH/2),
+            100,
+            0,
+            Math.PI * 2
         );
         ctx.fill();
 
